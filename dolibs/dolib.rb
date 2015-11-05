26cec2be8836f27c2591168b9625c8938b62f6c8
Colby Shores <col2037184@maricopa.edu>
2015-11-05 13:09:18 -0700

    Added command buffer list & monkey patch for color
    Added a command buffer list.  Pass an array of commands and execute them over ssh.  Added monkey patching for additional text colors without the use of libraries.

--- dolibs/dolib.rb
+++ dolibs/dolib.rb
@@ -1,12 +1,35 @@
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def cyan
+    colorize(36)
+  end
+end
+
 module Puppetdo
 
   def Puppetdo.dhcpapi(hostquery)
-    hosts = Array.new
-    content = ''
-    content << open("http://dhcp.vvmedia.com/rest/hostname/#{hostquery}").read
-    JSON.parse(content).each do |content|
-      if content["hostname"] =~ /^#{hostquery}[\d+]\.vvmedia\.com/
-        hosts  << content["hostname"]
+    hosts = []
+    content = open("http://dhcp.vvmedia.com/rest/hostname/#{hostquery}").read
+    #if explicitly defined due to numerical ordering
+    if JSON.parse(content).count == 1
+      hosts << JSON.parse(content)[0]["hostname"]
+    else
+      #otherwise isolate the cluster group based on its ending numerical value
+      JSON.parse(content).each do |content|
+        if content["hostname"] =~ /^#{hostquery}[\d+]\.vvmedia\.com/
+          hosts  << content["hostname"]
+        end
       end
     end
     return hosts
@@ -14,12 +37,15 @@ module Puppetdo
 
   def Puppetdo.puppet(puppetquery)
     #put a list if all certificate host names from the second column that starts with the API query followed by a number and followed by .vvmedia.com in to a string array called certs...
-    certs = `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}[[:digit:]]+\.vvmedia\.com'`.delete('"').split(/\n/)
-    return certs
+    certs = `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}.vvmedia\.com'`.delete('"').split(/\n/)
+    if certs.count == 1
+      return certs
+    else
+      return `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}[[:digit:]]+\.vvmedia\.com'`.delete('"').split(/\n/)
+    end
   end
 
   def Puppetdo.process(groupquery)
-    #align for auditing all machines
     $flag[:auditall] ? differences =  (Puppetdo.dhcpapi(groupquery) + Puppetdo.puppet(groupquery)).uniq : differences =  Puppetdo.dhcpapi(groupquery) - Puppetdo.puppet(groupquery)
     if $flag[:audit] or $flag[:auditall]
        if differences.to_s != '' or $flag[:auditall]
@@ -35,29 +61,43 @@ module Puppetdo
          puts "\n\n"
        end
     else
-      processing(groupquery)
+      processing(groupquery){ @commands }
     end
   end
+
+  def Puppetdo.commands(commands)
+    @commands = commands
+  end
+
 end
 
 
 def processing(groupquery)
-
   hostlist = Puppetdo.puppet(groupquery)
   if hostlist.to_s != ''
     puts "Processing #{groupquery} hosts...\n"
     hostlist.each do |hostname|
       begin
-
-          puts "starting... #{hostname}"
-          ssh = Net::SSH.start(hostname, 'root')
-          if $flag[:noop]
-            sshcommand = "puppet agent --test --noop"
-          else
+        puts "starting... #{hostname}\n"
+        ssh = Net::SSH.start(hostname, 'root', :paranoid => false)
+        if $flag[:noop]
+          sshcommand = "puppet agent --test --noop"
+        else
+          if !yield
             sshcommand = "puppet agent --test"
             res = ssh.exec!(sshcommand)
-         end
-
+          else
+            sshcommands = yield
+            res = ''
+            sshcommands.each do |command|
+              begin
+                res << ssh.exec!(command).green
+              rescue
+                res << "#{command} <- executed".cyan + "\n"
+              end
+            end
+          end
+        end
         ssh.close
         if $flag[:verbose] or $flag[:noop]
           puts res
@@ -69,6 +109,8 @@ def processing(groupquery)
       sleep 1
     end
   end
+  remove_instance_variable(:@commands)  #clear command buffer
+  rescue
 end
 
 def ping(host, timeout=5, service="echo")
@@ -83,4 +125,3 @@ def ping(host, timeout=5, service="echo")
     return false
   end
 end
-
