1496403baa3dad4e0b1b773ee729ef048cd4f21b
Colby Shores <col2037184@maricopa.edu>
2016-01-06 09:52:46 -0700

    Update includes.rb
    Added a a global that contains every variable in hiera to be referenced elsewhere in the puppetdo library.

--- dolibs/includes.rb
+++ dolibs/includes.rb
@@ -7,3 +7,5 @@ require 'puppet'
 require 'etc'
 require "/etc/puppet/dolibs/dolib.rb"
 require "/etc/puppet/dolibs/args.rb"
+$scope = YAML.load_file("/etc/puppet/hieradata/common.yaml").values
+$hiera = Hiera.new(:config => "/etc/puppet/hiera.yaml")
