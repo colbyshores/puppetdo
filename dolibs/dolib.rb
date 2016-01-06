6b732d398dcce025823029be77342679469c5428
Colby Shores <col2037184@maricopa.edu>
2016-01-06 09:50:56 -0700

    Update dolib.rb
    Added the numerical sorting algorithm as a monkey patch for the Array class.  Added a few hiera instances to look up the primary domain name for each server in a cluster as "domain" and the dhcp server domain from hiera as "dhcpserver"
    
    Added further granularity to auditing all the systems in the cluster to be piped through the aha command line tool to be emailed.

--- dolibs/dolib.rb
+++ dolibs/dolib.rb
@@ -14,20 +14,33 @@ class String
   def cyan
     colorize(36)
   end
+
+  def yellow
+    colorize(33)
+  end
+end
+
+class Array
+  def sort_by_number
+    self.sort_by{|i|i.gsub(/\d+/) {|s| "%08d" % s.to_i }}
+  end
 end
 
 module Puppetdo
 
   def Puppetdo.dhcpapi(hostquery)
+    dhcpserver = $hiera.lookup("dhcpserver","/",$scope)
+    domain = $hiera.lookup("domain","/",$scope)
     hosts = []
-    content = open("http://dhcp.vvmedia.com/rest/hostname/#{hostquery}").read
+    content = open("http://#{dhcpserver}/rest/hostname/#{hostquery}").read
+
     #if explicitly defined due to numerical ordering
     if JSON.parse(content).count == 1
       hosts << JSON.parse(content)[0]["hostname"]
     else
       #otherwise isolate the cluster group based on its ending numerical value
       JSON.parse(content).each do |content|
-        if content["hostname"] =~ /^#{hostquery}[\d+]\.vvmedia\.com/
+        if content["hostname"] =~ /^#{hostquery}[\d+]\.#{domain}/
           hosts  << content["hostname"]
         end
       end
@@ -35,30 +48,63 @@ module Puppetdo
     return hosts
   end
 
+  def Puppetdo.dhcpapi_byip(ipaddress)
+    dhcpserver = $hiera.lookup("dhcpserver","/",$scope)
+    host = ''
+    content = open("http://#{dhcpserver}/rest/ipaddress/#{ipaddress}").read
+      JSON.parse(content).each do |content|
+      if content["ipaddress"] =~ /^#{ipaddress}$/
+        host  << content["hostname"]
+        break
+      end
+    end
+    return host
+  end
+
   def Puppetdo.puppet(puppetquery)
-    #put a list if all certificate host names from the second column that starts with the API query followed by a number and followed by .vvmedia.com in to a string array called certs...
-    certs = `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}.vvmedia\.com'`.delete('"').split(/\n/)
+    domain = $hiera.lookup("domain","/",$scope)
+    #put a list if all certificate host names from the second column that starts with the API query followed by a number and followed by the primary domain in to a string array called certs...
+    certs = `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}.#{domain}'`.delete('"').split(/\n/)
     if certs.count == 1
       return certs
     else
-      return `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}[[:digit:]]+\.vvmedia\.com'`.delete('"').split(/\n/)
+      return `puppet cert list --all | awk '{ print $2 }' | egrep '^\"#{puppetquery}[[:digit:]]+\.#{domain}'`.delete('"').split(/\n/)
     end
   end
 
   def Puppetdo.process(groupquery)
-    $flag[:auditall] ? differences =  (Puppetdo.dhcpapi(groupquery) + Puppetdo.puppet(groupquery)).uniq : differences =  Puppetdo.dhcpapi(groupquery) - Puppetdo.puppet(groupquery)
+    $flag[:auditall] ? differences =  (Puppetdo.dhcpapi(groupquery) + Puppetdo.puppet(groupquery)).uniq.sort_by_number : differences =  (Puppetdo.dhcpapi(groupquery) - Puppetdo.puppet(groupquery)).sort_by_number
     if $flag[:audit] or $flag[:auditall]
        if differences.to_s != '' or $flag[:auditall]
-         $flag[:auditall] ? "Auditing all hosts on #{groupquery}:\n" : "These hosts on the #{groupquery} group do not match Puppet Cert -> DHCP:\n"
          counter = 0
          differences.each do |host|
            Puppetdo.puppet(groupquery).include?(host) ? puppetresult = 'puppetcert->true'.green : puppetresult = 'puppetcert->false'.red
            Puppetdo.dhcpapi(groupquery).include?(host) ? dhcpresult = 'dhcp->true'.green : dhcpresult = 'dhcp->false'.red
            ping(host) ? pingresult = 'ping->true'.green : pingresult = 'ping->false'.red
-           puts differences[counter] + " = #{puppetresult}  #{dhcpresult}  #{pingresult}"
+
+           begin
+             if host != Puppetdo.dhcpapi_byip(IPSocket.getaddress(host))
+               ipaddress = "A->#{IPSocket.getaddress(host)}:DHCPMismatch".red
+             else
+               ipaddress = "A->#{IPSocket.getaddress(host)}".green
+             end
+           rescue
+             ipaddress = "A->None".red
+           end
+
+           begin
+             if Resolv.getname(IPSocket.getaddress(host)) != host
+               reverse = "PTR->#{Resolv.getname(IPSocket.getaddress(host))}".red
+             else
+               reverse = "PTR->#{Resolv.getname(IPSocket.getaddress(host))}".green
+             end
+           rescue
+             reverse = "PTR->None".yellow
+           end
+
+           puts "#{differences[counter]} = #{puppetresult}  #{dhcpresult}  #{pingresult}  #{ipaddress}  #{reverse}"
            counter += 1
          end
-         puts "\n\n"
        end
     else
       processing(groupquery){ @commands }
@@ -68,12 +114,11 @@ module Puppetdo
   def Puppetdo.commands(commands)
     @commands = commands
   end
-
 end
 
 
 def processing(groupquery)
-  hostlist = Puppetdo.puppet(groupquery)
+  hostlist = Puppetdo.puppet(groupquery).sort_by_number
   if hostlist.to_s != ''
     puts "Processing #{groupquery} hosts...\n"
     hostlist.each do |hostname|
